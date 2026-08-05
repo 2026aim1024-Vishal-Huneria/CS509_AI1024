515830390e4acb5fa7ecb32bdc02431c66813584
2026aim1024-Vishal-Huneria <2026aim1024@iitrpr.ac.in>
2026-08-05 22:54:59 +0530

    added csr

--- CSR_Graph/csr_graph.cpp
+++ CSR_Graph/csr_graph.cpp
@@ -16,15 +16,48 @@ void printArray(int arr[], int sz)
 
 int main()
 {
+     int choice;
+    string fileName;
 
-    ifstream fin("graph_test_01.txt");
+    cout << "1. Run Test 01\n";
+    cout << "2. Run Test 02\n";
+    cout << "3. Run Test 03\n";
+    cout << "4. Run Test 04\n\n";
 
+    cout << "Enter Choice : ";
+    cin >> choice;
+
+    switch(choice)
+    {
+        case 1:
+    fileName = "tests/graph_test_01.txt";
+    break;
+
+case 2:
+    fileName = "tests/graph_test_02.txt";
+    break;
+
+case 3:
+    fileName = "tests/graph_test_03.txt";
+    break;
+
+case 4:
+    fileName = "tests/graph_test_04.txt";
+    break;
+
+        default:
+            cout << "Invalid Choice\n";
+            return 0;
+    }
+
+    ifstream fin(fileName);
 
     if(!fin)
     {
-        cout<<"File not found\n";
+        cout << "Cannot open file\n";
         return 0;
     }
+
 
 
     int vertices, edges;
